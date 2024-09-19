a7e3457bbddff2eb560e3d090f2f31493a9ae544
DABUSS <david.buss@btc-it-services.com>
2024-09-19 10:15:04 +0200

    Avatar verschönert

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,63 +2,64 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        boolean bool = false;
-        String robotArt =
-                "  +--------+\n" +
-                        "  |  [::]  |\n" +
-                        "  |  [::]  |\n" +
-                        "  |  [::]  |\n" +
-                        "  +--------+\n" +
-                        "  |  [__]  |\n" +
-                        "  |        |\n" +
-                        "  |  ROBOT |\n" +
-                        "  |  WARS  |\n" +
-                        "  +--------+\n" +
-                        "     |  |\n" +
-                        "    /    \\\n" +
-                        "   |      |\n" +
-                        "   |______|\n";
 
+        String robotArt = "  +--------+\n" +
+                "  |  [::]  |\n" +
+                "  |  [::]  |\n" +
+                "  |  [::]  |\n" +
+                "  +--------+\n" +
+                "  |  [__]  |\n" +
+                "  |        |\n" +
+                "  |  ROBOT |\n" +
+                "  |  WARS  |\n" +
+                "  +--------+\n" +
+                "     |  |\n" +
+                "    /    \\\n" +
+                "   |      |\n" +
+                "   |______|\n";
         System.out.println(robotArt);
+
         System.out.println("Willkommen bei Robot-Wars, wähle einen Namen für deinen Roboter");
         Scanner scanner = new Scanner(System.in);
+
         String name = scanner.nextLine();
-        while (bool == false){
-        if (name.contains(" ")) {
-        }
-        else if (name.contains(".")) {
-        }
-        else if (name.contains(",")) {}
-        else if (name.contains("-")) {}
-        else if (name.contains("!")) {}
-        else if (name.contains("§")) {}
-        else if (name.contains("$")) {}
-        else if (name.contains("%")) {}
-        else if (name.contains("&")) {}
-        else if (name.contains("/")) {}
-        else if (name.contains("(")) {}
-        else if (name.contains(")")) {}
-        else if (name.contains("=")) {}
-        else if (name.contains("?")) {}
-        else if (name.contains("´")) {}
-        else if (name.contains("`")) {}
-        else if (name.contains("+")) {}
-        else if (name.contains("*")) {}
-        else if (name.contains("'")) {}
-        else if (name.contains("#")) {}
-        else if (name.contains(";")) {}
-        else if (name.contains(":")) {}
-        else if (name.contains("_")) {}
-        else if (name.contains("^")) {}
-        else if (name.contains(">")) {}
-        else if (name.contains("<")) {}
-        else {
-            bool = true;
-        }
-        if (bool == false){
-            System.out.println("Dieser Name enthält verbotene Zeichen (bitte nur Zahlen und Buchstaben)");
-            System.out.println("Gebe einen anderen Namen ein");
-            name = scanner.nextLine();}
+        boolean bool = false;
+
+        while (bool == false) {
+            if (name.contains(" ")) {
+            } else if (name.contains(".")) {
+            } else if (name.contains(",")) {
+            } else if (name.contains("-")) {
+            } else if (name.contains("!")) {
+            } else if (name.contains("§")) {
+            } else if (name.contains("$")) {
+            } else if (name.contains("%")) {
+            } else if (name.contains("&")) {
+            } else if (name.contains("/")) {
+            } else if (name.contains("(")) {
+            } else if (name.contains(")")) {
+            } else if (name.contains("=")) {
+            } else if (name.contains("?")) {
+            } else if (name.contains("´")) {
+            } else if (name.contains("`")) {
+            } else if (name.contains("+")) {
+            } else if (name.contains("*")) {
+            } else if (name.contains("'")) {
+            } else if (name.contains("#")) {
+            } else if (name.contains(";")) {
+            } else if (name.contains(":")) {
+            } else if (name.contains("_")) {
+            } else if (name.contains("^")) {
+            } else if (name.contains(">")) {
+            } else if (name.contains("<")) {
+            } else {
+                bool = true;
+            }
+            if (bool == false) {
+                System.out.println("Dieser Name enthält verbotene Zeichen (bitte nur Zahlen und Buchstaben)");
+                System.out.println("Gebe einen anderen Namen ein");
+                name = scanner.nextLine();
+            }
         }
         System.out.println("Dein Roboter heißt: " + name);
     }
