3346e6fdee178fd05536068389295845c58312e7
DABUSS <david.buss@btc-it-services.com>
2024-09-19 14:32:37 +0200

    map 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,6 +18,7 @@ public class Main {
                 "    /    \\\n" +
                 "   |      |\n" +
                 "   |______|\n";
+
         System.out.println(robotArt);
 
         System.out.println("Willkommen bei Robot-Wars, wähle einen Namen für deinen Roboter");
@@ -56,26 +57,70 @@ public class Main {
             } else {
                 bool = true;
             }
+
             if (bool == false) {
                 System.out.println("Dieser Name enthält verbotene Zeichen (bitte nur Zahlen und Buchstaben)");
                 System.out.println("Gebe einen anderen Namen ein");
                 name = scanner.nextLine();
             }
         }
+
         System.out.println("Dein Roboter heißt: " + name);
 
-        int x = 0;
-        int y = 0;
+        boolean bool2 = false;
+        boolean bool3 = false;
 
-        while (y < 10) {
-            while (x < 14) {
-                System.out.print("[ ]");
-                x = x + 1;
+        System.out.println("Wähle eine X-Koordinate");
+        int xk = scanner.nextInt();
+
+        while (bool2 == false) {
+            if (xk <= 15 && xk >= 0) {
+                bool2 = true;
+            } else {
+                System.out.println("Wähle eine Zahl im Spielfeld (15x10)");
+                xk = scanner.nextInt();
+            }
+        }
+        System.out.println("wähle eine Y-Koordinate");
+        int yk = scanner.nextInt();
+
+        while (bool3 == false) {
+            if (yk <= 10 && yk >= 0) {
+                bool3 = true;
+            } else {
+                System.out.println("Wähle eine Zahl im Spielfeld (15x10)");
+                yk = scanner.nextInt();
+            }
+        }
+
+        int a = 0;
+        int b = 0;
+        int x = xk - 1;
+        int y = yk;
+
+        while (b < 12) {
+            while (a < 16) {
+                if (a == 0 || b == 0 || b == 11) {
+                    System.out.print("(X)");
+                } else {
+                    System.out.print("[ ]");
+                }
+
+                if (a == x && b == y) {
+                    System.out.print("{O}");
+                    a = a + 1;
+                }
+                a = a + 1;
                 TimeUnit.MILLISECONDS.sleep(50);
             }
-            System.out.println("[ ]");
-            y = y + 1;
-            x= 0;
+            System.out.println("(X)");
+            b = b + 1;
+            a = 0;
         }
+
+        System.out.println("Der Roboter " + name + " befindet sich auf der X-Koordinate:");
+        System.out.println(xk);
+        System.out.println("und auf der Y-Koordinate:");
+        System.out.println(yk);
     }
 }
